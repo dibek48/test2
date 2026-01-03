45124a26bf2e09afe86f681c74a36eecc0b93371
Ömer Umut DİBEK <dibek.exe@gmail.com>
2026-01-03 22:17:40 +0300

    update

---
+++ test.py
@@ -0,0 +1,27 @@
+def test(*args, **kwargs):
+    notification_id = request.view_args.get('id', 0)
+    subject = _('Notification')
+    status = NotificationStatus.SEEN.value
+    if notification_id != 0:
+        notification = Notification.query.get(notification_id)
+        if notification:
+            subject = notification.subject
+            status = notification.notification_status
+            
+def test(*args, **kwargs):
+    notification_id = request.view_args.get('id', 0)
+    subject = _('Notification')
+    status = NotificationStatus.SEEN.value
+    if notification_id != 0:
+        notification = Notification.query.get(notification_id)
+        if notification:
+            subject = notification.subject
+            status = notification.notification_statusdef test(*args, **kwargs):
+    notification_id = request.view_args.get('id', 0)
+    subject = _('Notification')
+    status = NotificationStatus.SEEN.value
+    if notification_id != 0:
+        notification = Notification.query.get(notification_id)
+        if notification:
+            subject = notification.subject
+            status = notification.notification_status
